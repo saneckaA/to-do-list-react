5b810fb45e091de4706a8d8d770dc03cacfad705
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-12 11:17:56 +0200

    connecting saga to store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
+import rootSaga from "./rootSaga";
 import tasksReducer from "./features/tasks/tasksSlice";
 
+
+const sagaMiddleware = createSagaMiddleware();
 export default configureStore({
     reducer: {
         tasks: tasksReducer,
     },
+    middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(rootSaga);
