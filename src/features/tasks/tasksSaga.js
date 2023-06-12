3d867139942b1f25abd494bd3c9c9d65a67f8070
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-12 11:14:27 +0200

    added root and tasks saga

---
+++ src/features/tasks/tasksSaga.js
@@ -0,0 +1,22 @@
+import {takeEvery, call, put, takeLatest, select} from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+import { fetchExampleTasks, setTasks, selectTasks } from "./tasksSlice";
+
+function* fetchExampleTasksHandler() {
+    try{
+        const exampleTasks = yield call(getExampleTasks);
+        yield put(setTasks(exampleTasks));
+    } catch (error) {
+      yield call(alert, "Coś poszło nie tak")
+    }
+}
+function* saveTasksInLocalStorageHandler() {
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocalStorage, tasks);
+}
+
+export function* tasksSaga() {
+    yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+    yield takeEvery("*", saveTasksInLocalStorageHandler);
+}
