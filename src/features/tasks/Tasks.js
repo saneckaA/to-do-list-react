967675e821021b445858bfbf79e8593b5bcf6e0b
Aleksandra Sanecka <120121837+saneckaA@users.noreply.github.com>
2023-06-27 17:31:10 +0200

    Update Tasks.js

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,4 +1,3 @@
-
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
@@ -6,20 +5,16 @@ import Section from "../../common/Section";
 import Header from "../../common/Header";
 import Container from "../../common/Container";
 
-
 function Tasks() {
-
   return (
     <Container>
       <Header
         title="Lista zadań"
       />
-
       <Section
         title="Dodaj nowe zadanie"
         body={<Form />}
       />
-
       <Section
         title="Lista zadań"
         body={<TaskList />}
