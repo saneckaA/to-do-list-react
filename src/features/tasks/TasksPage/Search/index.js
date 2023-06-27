0790b21284bd56063d389d14531361a7a8c69ded
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-27 18:52:29 +0200

    added search component

---
+++ src/features/tasks/TasksPage/Search/index.js
@@ -0,0 +1,27 @@
+import Input from "../../Input"
+import { Wrapper } from "./styled";
+import searchQueryParamName from "../searchQueryParamName";
+import { useQueryParameter, useReplaceQueryParameter } from "../queryParameters";
+
+const Search = () => {
+    const query = useQueryParameter(searchQueryParamName);
+    const replaceQueryParameter = useReplaceQueryParameter();
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter({
+            key: searchQueryParamName,
+            value: target.value.trim() !=="" ? target.value : undefined
+        });
+    };
+        return (
+            <Wrapper>
+                <Input
+                    placeholder="Filtruj zadania"
+                    value={query || ""}
+                    onChange={onInputChange}
+                />
+            </Wrapper>
+        );
+    };
+
+    export default Search;
