c9c606eff187ec6a7a9569e622756f0f22fb4b62
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-27 18:47:05 +0200

    added author page

---
+++ src/features/author/Author.js
@@ -0,0 +1,23 @@
+import Section from "../../common/Section";
+import Header from "../../common/Header";
+import Container from "../../common/Container";
+import { Description } from "./styled";
+
+function Author() {
+  return (
+    <Container>
+      <Header
+        title="O autorze"
+      />
+      <Section
+        title="Aleksandra Sanecka"
+        body={<Description>Nazywam się Aleksandra Sanecka. Ukończyłam Politechnikę Rzeszowską na wydziale Chemicznym.
+          Studiowałam Biotechnologię w Diagnostyce Laboratoryjnej. Wolne chwile lubię
+          poświęcać na malarstwo cyfrowe, naukę języków czy od niedawna również na naukę
+          programowania. </Description> }
+      />
+    </Container>
+  );
+}
+
+export default Author;
---
+++ src/features/author/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Description = styled.span`
+   font-size: 16px;
+   line-height: 1.3;
+   text-align: justify;
+   text-indent: 20px;
+   display: block;
+`;
