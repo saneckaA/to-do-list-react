82e950bc3667fec70fe0b95cc984e0863fa403d6
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-06-27 18:58:28 +0200

    added new reducers

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -27,19 +27,37 @@ const tasksSlice = createSlice({
                 task.done = true;
             }
         },
-        fetchExampleTasks: () => {},
-        setTasks: (state, {payload: tasks}) => {
+        fetchExampleTasks: state => {
+            state.loading = true;
+        },
+        fetchExampleTasksSuccess: (state, {payload: tasks}) => {
             state.tasks = tasks;
+            state.loading = false;
+        },
+        fetchExampleTasksError: (state) => {
+            state.loading = false;
         },
     },
 });
 
 const selectTasksState = state => state.tasks;
 
-export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, fetchExampleTasks, setTasks, } = tasksSlice.actions;
+export const { addTask, toggleHideDone, toggleTaskDone, removeTask, setAllDone, fetchExampleTasks, fetchExampleTasksSuccess, fetchExampleTasksError } = tasksSlice.actions;
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 export const selectHideDone = state => selectTasksState(state).hideDone;
+export const selectLoading = state => selectTasksState(state).loading;
+
+export const getTaskById = (state, taskId) => selectTasks(state).find(({id}) => id === taskId);
+
+export const selectTasksByQuery = (state, query) => {
+const tasks = selectTasks(state);
+if(!query || query.trim() === "") {
+    return tasks;
+}
+return tasks.filter(({content}) => content.toUpperCase().includes(query.trim().toUpperCase()));
+};
 
 export default tasksSlice.reducer;
+
